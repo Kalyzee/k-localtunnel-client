b06e29b45ff6721822c10f59871abe7949878e27
Dorian Rouby <dorian.rouby@gmail.com>
2026-03-06 16:16:04 +0100

    feat: added static tcp tunnel server && update credentials

--- test.js
+++ test.js
@@ -1,13 +1,36 @@
 const localtunnel = require('./localtunnel');
 
-(async () => {
-  const tunnel = await localtunnel({
-    port: 8080,                     // le port de ton serveur local
-    host: 'http://localhost:3001',   // ton serveur localtunnel custom
-    authKey: 'abcd1234',
-    clientId: 'device-1'
-  });
+const run = (async () => {
+  let tunnel;
+  try {
+    tunnel = await localtunnel({
+      port: 3000,                     // le port de ton serveur local
+      host: 'https://tunnel.dev.kast.app',   // ton serveur localtunnel custom
+      authKey: 'abcd1234',
+      clientId: 'device-1',
+      staticTcpTunnel: {
+        tls: true,
+        host: 'socket-tunnel.dev.kast.app',
+        port: 443
+      }
+    });
+    /*tunnel = await localtunnel({
+      port: 3000,                     // le port de ton serveur local
+      host: 'http://localhost:3001',   // ton serveur localtunnel custom
+      authKey: 'abcd1234',
+      clientId: 'device-2'
+    });*/
+  } catch(err) {
+    console.error("Error connection : ", err)
+  }
 
+  if (!tunnel) {
+    setTimeout(() => {
+      // retry
+      run();
+    }, 1000);
+    return;
+  }
   // the assigned public url for your tunnel
   // i.e. https://abcdefgjhij.localtunnel.me
   console.log("tunnel : ", tunnel)
@@ -17,8 +40,21 @@ const localtunnel = require('./localtunnel');
 
   });
 
+  tunnel.on("error", (err) => {
+    console.error("Error : ", err);
+    try {
+      tunnel.close();
+    } catch(_) {};
+    setTimeout(() => {
+      // retry
+      run();
+    }, 1000);
+  })
+
   tunnel.on('close', () => {
     // tunnels are closed
     console.log("close");
   });
-})();
+});
+
+run();
